d4514a84d6103c0e420b1f16dd795c1b552aab8d
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2022-02-10 15:27:27 +0100

    install eslint and prettier, get images

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,19 @@
     async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+        //récupère les informations sur chaque photographe
+  try {
+    const response = await fetch("./data/photographers.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+
+    const photographers = await response.json();
+    return photographers;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
@@ -36,10 +26,15 @@
     };
 
     async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
+        try {
+          // Récupère les datas des photographes
+          const { photographers } = await getPhotographers();
+          displayData(photographers);
+        } catch (err) {
+          // eslint-disable-next-line no-alert
+          alert("erreur systéme, le fichier json comporte des erreurs");
+        }
+    }
     init();
+      
     
