2cd0bd007a0c60d851867114af9e902ebda7eacb
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2022-03-10 14:52:34 +0100

    polyfill et wicg-inert installation

--- scripts/utils/dropdown.js
+++ scripts/utils/dropdown.js
@@ -1,24 +1,20 @@
 /** ****fonctionnements du dropdown et filtres*********** */
 
 const dropdownMenu = document.getElementById('dropdown_menu');
-// const menu = document.getElementsByClassName('dropdown-menu');
+const menu = document.getElementsByClassName('dropdown-menu');
 
 // ouverture du dropdown au click du bouton popularité:
-function dropdownToggle() {
-  if (dropdownMenu.classList.contains('visible')) {
-    dropdownMenu.classList.remove('visible');
-  } else {
-    dropdownMenu.classList.add('visible');
-  }
+function openDropdown() {
+  dropdownMenu.style.display = 'block';
 }
 
 // ferme le dropdown menu au click exterieur ou à l'interieur du menu
-/* window.onclick = (event) => {
+window.onclick = (event) => {
   if (!event.target.matches('.dropdown-button')) {
     let i;
     for (i = 0; i < menu.length; i += 1) {
-      const openDropdown = menu[i];
-      openDropdown.style.display = 'none';
+      const openMenu = menu[i];
+      openMenu.style.display = 'none';
     }
   }
-} */
+};
